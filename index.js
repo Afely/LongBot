c621de2229ca5f786a96591eba1481e156338032
Afely <67795820+Afely@users.noreply.github.com>
2020-08-13 01:30:30 +0700

    1.2
    :sweatsmile:

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const prefix = "c!";
 const player = "<:head:739022533918982174>"
 const bodyH = "<:bodyH:739022604462981150>"
 const bodyV = "<:bodyV:739022629415026810>"
+const bodyC = "<:bodyC:741217740089720884>"
+const bodyC1 = "<:bodyC1:743067145193390229>"
+const bodyC2 = "<:bodyC2:743067201577287681>"
+const bodyC3 = "<:bodyC3:743067175715340398>"
+
 var wallH = "<:wallH:739022697228402699>"
 var wallV = "<:wallV:739022724113891409>"
 var wallC = "<:wallC:739022656837517324>"
@@ -57,17 +62,18 @@ var levels = {
 30:"wc wh wh wh wh wh wh wc R wv bg bg bg bg bg bg wv R wv bg bg bg w w bg wv R wc w bg bg w P bg wv R wc w bg bg bg bg bg wv R wv bg bg w bg bg bg wv R wv bg bg bg bg bg bg wv R wc wh wh wh wh wh wh wc #0", 
 
 
-31:"", 
-32:"", 
-33:"", 
-34:"", 
-35:"", 
+31:"wc wh wh wh wh wh wh wh wc R wv bg bg bg bg bg bg bg wv R wv bg bg bg bg bg bg bg wv R wv bg bg bg bg bg P bg wv R wv bg bg bg bg bg bg bg wv R wv bg bg bg bg bg bg bg wv R wv bg bg bg bg bg bg bg wv R wc wh wh wh wh wh wh wh wc #1", 
+32:"wc wh wh wh wh wh wh wh wc R wv bg bg bg bg bg bg bg wv R wv bg w w w bg bg bg wv R wv bg bg bg w bg bg bg wv R wv bg bg bg w bg P bg wv R wv bg w bg w bg w bg wv R wv bg bg bg bg bg w bg wv R wv bg bg w bg bg bg bg wv R wc wh wh wc wh wh wh wh wc #1", 
+33:"w w w w wc wh wh wh wc R wc wh wh wh wc bg bg bg wv R wv bg bg bg bg bg P bg wv R wv bg bg w bg bg bg bg wv R wv bg bg bg w bg bg bg wv R wv bg w bg bg bg w bg wv R wv bg bg bg bg bg bg bg wv R wv bg bg w w bg bg wc wc R wc wh wh wc wc wh wh wc w #1", 
+34:"wc wh wh wh wh wh wh wh wc R wv bg bg bg bg bg bg bg wv R wv bg w w w w bg bg wv R wv bg bg bg bg bg bg w wc R wc wc P bg bg bg bg w wc R w wv bg bg w w bg bg wv R w wv bg bg bg bg bg bg wv R w wv bg bg bg bg bg bg wv R w wc wh wh wh wh wh wh wc #1", 
+35:"wc wh wh wh wc wh wh wh wc R wv bg bg bg w bg bg bg wv R wv bg w bg bg bg w bg wv R wv bg w w bg bg bg bg wv R wv bg bg bg bg w bg w wv R wv bg bg w bg P bg bg wv R wc wc bg bg bg bg bg bg wv R w wc wc bg bg wc wh wh wc R w w wc wh wh wc w w w #1", 
 
-36:"", 
-37:"", 
-38:"", 
-39:"", 
-40:"", 
+36:"wc wh wh wh wh wh wh wc w R wv bg bg bg bg bg bg wv w R wv bg bg P bg w bg wc wc R wc wc bg bg bg w bg bg wv R w wv bg bg bg bg w bg wv R w wv bg bg bg bg w bg wv R w wc wc bg bg bg w bg wv R w w wv bg w bg bg bg wv R w w wc wh wc wh wh wh wc #2", 
+37:"wc wh wh wc wh wh wh wc w R wv bg bg w bg bg bg wc wc R wv bg bg bg bg w bg bg wv R wv bg w bg bg bg bg bg wv R wv bg bg bg w bg P bg wv R wc wc bg bg w bg bg bg wv R w wv bg bg bg bg bg bg wv R w wv bg bg bg bg bg bg wv R w wc wh wh wh wh wh wh wc #2", 
+38:"wc wh wh wh wc wh wh wh wc R wv bg bg bg w bg bg bg wv R wv bg w bg w bg P bg wv R wv bg bg bg bg bg bg bg wv R wc w bg bg bg bg bg bg wv R wv bg bg bg bg bg w bg wv R wv bg w w w bg bg bg wv R wv bg bg bg bg bg bg bg wv R wc wh wh wh wh wh wh wh wc #2", 
+39:"wc wh wh wc wh wh wh wh wc R wv bg bg w bg bg bg bg wv R wv bg bg w bg bg w bg wv R wv bg bg bg bg bg bg bg wv R wv bg bg bg P bg w w wc R wv bg bg bg bg bg bg bg wv R wv bg bg bg bg w bg bg wv R wv bg bg bg bg bg bg wc wc R wc wh wh wh wh wh wh wc w #2", 
+40:"wc wh wh wh wh wh wh wh wc R wv bg bg bg bg bg bg bg wv R wv bg bg bg bg bg P bg wv R wv bg bg bg bg bg bg bg wv R wv bg w bg w w bg bg wv R wv bg bg bg w w bg bg wv R wv bg bg bg bg bg bg bg wv R wc wh wh wh wh wh wh wh wc #2", 
+
 
 41:"", 
 42:"", 
@@ -88,11 +94,12 @@ bot.on("ready", () =>{console.log("LongBot is online."); bot.user.setActivity("c
 
 var timer
 bot.on("message", msg =>{
-    if(msg.author.id == bot.user.id && msg.embeds[0].title.includes("•")){
-        reacted = false
-        timer = setTimeout(bebe = () =>{reacted = true}, 6000)
-        msg.react("🔼"); msg.react("🔽"); msg.react("◀️"); msg.react("▶️"); msg.react("🔄"); msg.react("⏭️");
-    }
+    if(msg.embeds[0]){
+        if(msg.author.id == bot.user.id && msg.embeds[0].title.includes("•")){
+            reacted = false
+            timer = setTimeout(bebe = () =>{reacted = true}, 6500)
+            msg.react("🔼"); msg.react("🔽"); msg.react("◀️"); msg.react("▶️"); msg.react("🔄"); msg.react("⏭️");
+    }}
     if(msg.author.bot) return;
     mbsg = msg
     let args = msg.content.substring(2).split(" ");
@@ -151,12 +158,13 @@ bot.on("message", msg =>{
             const helpembed = new Discord.MessageEmbed()
             .setTitle("LongBot - Longcat for Discord")
             .setColor(0xF5BA00)
-            .addField("Commands", "-c!help - Posts this help board.\n-c!play (level) - Start a game at level 1 or specified level.\n-c!reset - Resets current level.\n-c!next - Continue to next level.")
-            .addField("Notes", "Version 1.1\nBot created by Afely\nFancade & Longcat created by Martin Magni\nAfely's depression created by Javascript\nCheck out PolyMars' video where he created Sokobot, which\nwas my inspiration for this project!\nyoutu.be/0fWdU8JCT6Y\n\nIf you find any bugs, please post them in\nthe LongBot Discord server.")
+            .addField("Commands", "-c!help - Posts this help board.\n-c!play (level) - Start a game at level 1 or specified level.\n-c!reset - Resets current level.\n-c!next - Continue to next level.\n-c!end - Ends current game.")
+            .addField("Notes", "Version 1.2\nBot created by Afely\nFancade & Longcat created by Martin Magni\nAfely's depression created by Javascript\nCheck out PolyMars' video where he created Sokobot, which\nwas my inspiration for this project!\nhttps://youtu.be/0fWdU8JCT6Y\n\nIf you find any bugs, please post them in\nthe LongBot Discord server.")
+            .addField("Links", "LongBot Invite Link\nhttps://discord.com/oauth2/authorize?client_id=737401319408795710&scope=bot&permissions=8\n\nLongBot Discord Server\nhttps://discord.gg/2J9VYmQ")
+            .setThumbnail("https://media.discordapp.net/attachments/714769600125992984/743167174910410883/head.png")
             msg.channel.send(helpembed)
             break;
         case "play":
-            if(msg.guild.id == "739416043637702667" && !msg.channel.name.startsWith("play")) return;
             if(!games[msg.author.id]){
             games[msg.author.id] = new game(msg.author.username, msg.author.id, msg)
             }
@@ -169,6 +177,10 @@ bot.on("message", msg =>{
         case "next":
             if(!games[msg.author.id] || !reacted) return;
             games[msg.author.id].next()
+        case "end":
+            if(!games[msg.author.id] || !reacted) return;
+            games[msg.author.id] = undefined
+            msg.channel.send("<@" + msg.author.id + "> - Game ended")
     }
 })
 
@@ -184,13 +196,20 @@ class game{
         this.lvlsize
         this.timeout
         this.playing
+        this.h
     }
             d(){
                 if(!this.playing || this.msg.author.id != this.plrID) return;
                 this.lvlview = this.lvlcode.split(" ")
+                this.lvlsize = this.lvlcode.substr(0, this.lvlview.indexOf("R")).length + 1
+                this.h = false
                 while(this.lvlview[this.lvlview.indexOf("P") + 1] == "bg"){
                     this.lvlview[this.lvlview.indexOf("P") + 1] = "P"
-                    this.lvlview[this.lvlview.indexOf("P")] = "bh"
+                    if(!this.h){
+                        if(this.lvlview[this.lvlview.indexOf("P") + this.lvlsize] == "bv" || this.lvlview[this.lvlview.indexOf("P") + this.lvlsize].startsWith("bc")) this.lvlview[this.lvlview.indexOf("P")] = "bc1"
+                        else this.lvlview[this.lvlview.indexOf("P")] = "bc3"
+                        this.h = true
+                    }else this.lvlview[this.lvlview.indexOf("P")] = "bh"
                 }
                 this.lvlcode = this.lvlview.toString()
                 this.lvlcode = this.lvlcode.replace(/,/g, " ")
@@ -199,9 +218,15 @@ class game{
             a(){
                 if(!this.playing || this.msg.author.id != this.plrID) return;
                 this.lvlview = this.lvlcode.split(" ")
+                this.lvlsize = this.lvlcode.substr(0, this.lvlview.indexOf("R")).length + 1
+                this.h = false
                 while(this.lvlview[this.lvlview.indexOf("P") - 1] == "bg"){
                     this.lvlview[this.lvlview.indexOf("P") - 1] = "P"
-                    this.lvlview[this.lvlview.lastIndexOf("P")] = "bh"
+                    if(!this.h){
+                        if(this.lvlview[this.lvlview.lastIndexOf("P") + this.lvlsize] == "bv" || this.lvlview[this.lvlview.indexOf("P") + this.lvlsize].startsWith("bc")) this.lvlview[this.lvlview.lastIndexOf("P")] = "bc"
+                        else this.lvlview[this.lvlview.lastIndexOf("P")] = "bc2"
+                        this.h = true
+                    }else this.lvlview[this.lvlview.lastIndexOf("P")] = "bh"
                 }
                 this.lvlcode = this.lvlview.toString()
                 this.lvlcode = this.lvlcode.replace(/,/g, " ")
@@ -211,9 +236,14 @@ class game{
                 if(!this.playing || this.msg.author.id != this.plrID) return;
                 this.lvlview = this.lvlcode.split(" ")
                 this.lvlsize = this.lvlcode.substr(0, this.lvlview.indexOf("R")).length + 1
+                this.h = false
                 while(this.lvlview[this.lvlview.indexOf("P") - this.lvlsize] == "bg"){
                     this.lvlview[this.lvlview.indexOf("P") - this.lvlsize] = "P"
-                    this.lvlview[this.lvlview.lastIndexOf("P")] = "bv"
+                    if(!this.h){
+                        if(this.lvlview[this.lvlview.lastIndexOf("P") + 1] == "bh" || this.lvlview[this.lvlview.indexOf("P") + 1].startsWith("bc")) this.lvlview[this.lvlview.lastIndexOf("P")] = "bc3"
+                        else this.lvlview[this.lvlview.lastIndexOf("P")] = "bc2"
+                        this.h = true
+                    }else this.lvlview[this.lvlview.lastIndexOf("P")] = "bv"
                 }
                 this.lvlcode = this.lvlview.toString()
                 this.lvlcode = this.lvlcode.replace(/,/g, " ")
@@ -223,9 +253,14 @@ class game{
                 if(!this.playing || this.msg.author.id != this.plrID) return;
                 this.lvlview = this.lvlcode.split(" ")
                 this.lvlsize = this.lvlcode.substr(0, this.lvlview.indexOf("R")).length + 1
+                this.h = false
                 while(this.lvlview[this.lvlview.indexOf("P") + this.lvlsize] == "bg"){
                     this.lvlview[this.lvlview.indexOf("P") + this.lvlsize] = "P"
-                    this.lvlview[this.lvlview.indexOf("P")] = "bv"
+                    if(!this.h){
+                        if(this.lvlview[this.lvlview.indexOf("P") + 1] == "bh" || this.lvlview[this.lvlview.indexOf("P") + 1].startsWith("bc")) this.lvlview[this.lvlview.indexOf("P")] = "bc1"
+                        else this.lvlview[this.lvlview.indexOf("P")] = "bc"
+                        this.h = true
+                    }else this.lvlview[this.lvlview.indexOf("P")] = "bv"
                 }
                 this.lvlcode = this.lvlview.toString()
                 this.lvlcode = this.lvlcode.replace(/,/g, " ")
@@ -264,24 +299,29 @@ bot.on("messageReactionAdd", (react, user) =>{
     switch(react.emoji.name){
         case "🔼":
             games[user.id].w()
+            react.message.delete()
             break;
         case "🔽":
             games[user.id].s()
+            react.message.delete()
             break;
         case "◀️":
             games[user.id].a()
+            react.message.delete()
             break;
         case "▶️":
             games[user.id].d()
+            react.message.delete()
             break;
         case "🔄":
             games[user.id].reset()
+            react.message.delete()
             break;
         case "⏭️":
             games[user.id].next()
+            react.message.delete()
             break;
     }
-    react.message.delete()
 })
 
 // level generation
@@ -361,6 +401,10 @@ read = (code, mesg, lvl) =>{
         if(bode[x] == "P") level.push(player)
         if(bode[x] == "bh") level.push(bodyH)
         if(bode[x] == "bv") level.push(bodyV)
+        if(bode[x] == "bc") level.push(bodyC)
+        if(bode[x] == "bc1") level.push(bodyC1)
+        if(bode[x] == "bc2") level.push(bodyC2)
+        if(bode[x] == "bc3") level.push(bodyC3)
         if(bode[x] == "w") level.push(wall)
         if(bode[x] == "wc") level.push(wallC)
         if(bode[x] == "R") level.push("\n")
@@ -368,16 +412,132 @@ read = (code, mesg, lvl) =>{
 
     level = level.toString().replace(/,/g, "")
 
+    // win message generation
+    let num = Math.floor(Math.random() * 12)
+    let winmsg = []
+    switch(num){
+        case 1:
+            winmsg.push("Expert")
+            break;
+        case 2:
+            winmsg.push("Terrific")
+            break;
+        case 3:
+            winmsg.push("Awesome")
+            break;
+        case 4:
+            winmsg.push("Marvelous")
+            break;
+        case 5:
+            winmsg.push("Fantastic")
+            break;
+        case 6:
+            winmsg.push("Good")
+            break;
+        case 7:
+            winmsg.push("Great")
+            break;
+        case 8:
+            winmsg.push("Nice")
+            break;
+        case 9:
+            winmsg.push("Wonderful")
+            break;
+        case 10:
+            winmsg.push("Splendid")
+            break;
+        case 11:
+            winmsg.push("Admirable")
+            break;
+    }
+    num = Math.floor(Math.random() * 10)
+    switch(num){
+        case 1:
+            winmsg.push("Feat")
+            break;
+        case 2:
+            winmsg.push("Job")
+            break;
+        case 3:
+            winmsg.push("Victory")
+            break;
+        case 4:
+            winmsg.push("Skill")
+            break;
+        case 5:
+            winmsg.push("Success")
+            break;
+        case 6:
+            winmsg.push("Finish")
+            break;
+        case 7:
+            winmsg.push("Win")
+            break;
+        case 8:
+            winmsg.push("Game")
+            break;
+        case 9:
+            winmsg.push("Achievement")
+            break;
+    }
+    winmsg = winmsg.toString().replace(/,/g, " ")
+
+    // loss message generation
+    num = Math.floor(Math.random() * 12)
+    let losemsg = ""
+    switch(num){
+        case 1:
+            losemsg = "So Close!"
+            break;
+        case 2:
+            losemsg = "Next Time!"
+            break;
+        case 3:
+            losemsg = "Don't Give Up!"
+            break;
+        case 4:
+            losemsg = "Close Call!"
+            break;
+        case 5:
+            losemsg = "You Can Do It!"
+            break;
+        case 6:
+            losemsg = "Almost There!"
+            break;
+        case 7:
+            losemsg = "Nice Try!"
+            break;
+        case 8:
+            losemsg = "Go Again!"
+            break;
+        case 9:
+            losemsg = "Almost Got It!"
+            break;
+        case 10:
+            losemsg = "Try Again!"
+            break;
+        case 11:
+            losemsg = "One More Time!"
+            break;
+    }
+
     // embed compiling
     const lvlembed = new Discord.MessageEmbed()
     .setColor(0xF5BA00)
     .setTitle(plr + " • LEVEL " + lvl)
     .setDescription(level + `\n\nWASD/"up", "down", "left", "right"/reactions to move`)
     .setFooter("Created by Afely\nThanks to Martin Magni & Fancade")
+    if(lvlembed.description.length > 2048){
+        lvlembed.setDescription(level.substr(0, 2048))
+        lvlembed.addField("_", `NOTE: This level is too big, so part of it has been removed.\nWASD/"up", "down", "left", "right"/reactions to move`)
+        if(loss) lvlembed.fields[0].value = "NOTE: This level is too big, so part of it has been removed.\n**YOU LOSE**\n" + losemsg + "\n\nc!reset or 🔄 to reset"
+        if(spaces == 0) lvlembed.fields[0].value = "NOTE: This level is too big, so part of it has been removed.\n**YOU WIN!**\n" + winmsg + "\n\nc!next or ⏭️ to continue"
+        return mesg.channel.send(lvlembed)
+    }
 
     // win/loss detection
-    if(loss) lvlembed.setDescription(level + "\n\n**YOU LOSE**\nc!reset or 🔄 to reset")
-    if(spaces == 0) lvlembed.setDescription(level + "\n\n**YOU WIN!**\nc!next or ⏭️ to continue")
+    if(loss) lvlembed.setDescription(level + "\n\n**YOU LOSE**\n" + losemsg + "\n\nc!reset or 🔄 to reset")
+    if(spaces == 0) lvlembed.setDescription(level + "\n\n**YOU WIN!**\n" + winmsg + "\n\nc!next or ⏭️ to continue")
     mesg.channel.send(lvlembed)
 }
 
